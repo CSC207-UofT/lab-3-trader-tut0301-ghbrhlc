91f843ed76bdbacf0eafaa102181f7713fe0677f
leya-595 <82251651+leya-595@users.noreply.github.com>
2021-09-27 15:48:44 -0400

    Completed Task 2

--- src/main/java/Donkey.java
+++ src/main/java/Donkey.java
@@ -1,8 +1,18 @@
 public class Donkey implements Tradable, Domesticatable {
 
+    private int maxSpeed;
+
+    public Donkey(){
+        this.maxSpeed = 1;
+    }
+
+    public void upgradeSpeed() {
+        this.maxSpeed++;
+    }
+
     @Override
     public int getPrice() {
-        return "750";
+        return 750;
     }
 
     @Override
